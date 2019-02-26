b60d9645789112a330b6a57a3fe22aa629cc78f3
huyunhai <huyunhai@keep.com>
2019-02-26 18:48:03 +0800

    修改build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ git submodule init
 git submodule update
 ./mvnw clean package -Dmaven.test.skip=true $1
 git rev-parse --short HEAD > skywalking-agent/VERSION
-tar -cf skywalking-agent.tar skywalking-agent
+cd skywalking-agent
+tar -cf ../skywalking-agent.tar *
