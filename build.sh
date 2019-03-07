77c7b06be241247cfaa88a77615434ab0a420d4f
huyunhai <huyunhai@keep.com>
2019-03-07 15:29:55 +0800

    按照环境变量APP_ENV获取collector-server

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git submodule init
 git submodule update
-./mvnw clean package -Dmaven.test.skip=true $1
+./mvnw clean package -Dmaven.test.skip=true
 git rev-parse --short=10 HEAD > skywalking-agent/VERSION
 cd skywalking-agent
 tar -cf ../skywalking-agent.tar *
