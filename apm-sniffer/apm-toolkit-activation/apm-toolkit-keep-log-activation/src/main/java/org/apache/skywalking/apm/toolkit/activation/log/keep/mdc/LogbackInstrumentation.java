57ecaaea2edd68b5b53795a2dbf16447e2857a1a
huyunhai <huyunhai@keep.com>
2019-04-03 16:09:01 +0800

    fix MDCInterceptor package name

--- apm-sniffer/apm-toolkit-activation/apm-toolkit-keep-log-activation/src/main/java/org/apache/skywalking/apm/toolkit/activation/log/keep/mdc/LogbackInstrumentation.java
+++ apm-sniffer/apm-toolkit-activation/apm-toolkit-keep-log-activation/src/main/java/org/apache/skywalking/apm/toolkit/activation/log/keep/mdc/LogbackInstrumentation.java
@@ -34,7 +34,7 @@ import static org.apache.skywalking.apm.agent.core.plugin.match.NameMatch.byName
 public class LogbackInstrumentation extends LogMDCInstrumentation {
 
     private static final String ENHANCE_CLASS = "ch.qos.logback.classic.LoggerContext";
-    private static final String INTERCEPTOR_CLASS = "org.apache.skywalking.apm.toolkit.activation.log.infra" +
+    private static final String INTERCEPTOR_CLASS = "org.apache.skywalking.apm.toolkit.activation.log.keep" +
             ".MDCConstructorInterceptor";
 
     @Override
